298c7db0afb65f69f8c21e37d0761066ad3567dd
shafik720 <shafikrasel5@gmail.com>
2022-11-09 23:31:18 +0600

    toggle login signup

--- src/App.js
+++ src/App.js
@@ -12,52 +12,59 @@ const auth = getAuth(app);
 
 function App() {
 
-  const[email, setEmail] = useState('');
-  const[password, setPassword] = useState('');
-  const[error, setError] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [registered, setRegistered] = useState(false);
 
-  function handleEmail(e){
+  function handleEmail(e) {
     setEmail(e.target.value);
   }
 
-  function handlePassword(e){
+  function handlePassword(e) {
     setPassword(e.target.value);
   }
 
-  function handleSubmit(e){
+  function handleCheckbox(e){
+    setRegistered(e.target.checked);
+  }
+
+  function handleSubmit(e) {
     e.preventDefault();
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      console.log(user);
-    })
-    .catch((error) => {
-      // console.log(error.message);
-      setError(error.message);
-    });
-  } 
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }
   return (
     <div>
       <div className="container my-5">
         <div className="row mt-5">
-            <div className="col-lg-5 mx-auto">
+          <div className="col-lg-5 mx-auto">
             <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <h2 className="text-primary text-center"> {registered ? 'Log In' : 'Sign Up' } </h2>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
                 <h5>Your Email</h5>
-                <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" />                
-            </Form.Group>
+                <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" />
+              </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <h5>Your Password</h5>
-            <Form.Control onBlur={handlePassword} type="password" placeholder="Password" />
-          </Form.Group>
-          <h4 className="text-danger">{error}</h4>
-          <Button onClick={handleSubmit} variant="primary" type="submit">
-            Submit
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <h5>Your Password</h5>
+                <Form.Control onBlur={handlePassword} type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check onChange={handleCheckbox} type="checkbox" label="Check me out" />
+              </Form.Group>
+              <h4 className="text-danger">{error}</h4>
+              <Button onClick={handleSubmit} variant="primary" type="submit">
+                Submit
           </Button>
-        </Form>
-            </div>
+            </Form>
+          </div>
         </div>
       </div>
     </div>
