3f881c66665a3af656eeb4e696faf7ef36f4d024
shafik720 <shafikrasel5@gmail.com>
2022-11-09 20:34:29 +0600

    function added to forms

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ import { Button, Form } from 'react-bootstrap';
 
 
 function App() {
-  
+
+  function handleEmail(e){
+    console.log(e.target.value);
+  }
+
+  function handlePassword(e){
+    console.log(e.target.value);
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+  } 
   return (
     <div>
       <div className="container my-5">
@@ -17,15 +28,15 @@ function App() {
             <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <h5>Your Email</h5>
-                <Form.Control type="email" placeholder="Enter email" />                
+                <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" />                
             </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <h5>Your Password</h5>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control onBlur={handlePassword} type="password" placeholder="Password" />
           </Form.Group>
           
-          <Button variant="primary" type="submit">
+          <Button onClick={handleSubmit} variant="primary" type="submit">
             Submit
           </Button>
         </Form>
