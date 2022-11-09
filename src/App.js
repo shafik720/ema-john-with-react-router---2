8350338aa6d7e58cd171889f2e5e9d387a7e1eaa
shafik720 <shafikrasel5@gmail.com>
2022-11-09 19:59:22 +0600

    form implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap';
 
 
 
@@ -9,9 +10,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   
   return (
-    <div className="App">
-      <h2>Hello</h2>
-      <button>Sign in with google</button>
+    <div>
+      <div className="container my-5">
+        <div className="row mt-5">
+            <div className="col-lg-5 mx-auto">
+            <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <h5>Your Email</h5>
+                <Form.Control type="email" placeholder="Enter email" />                
+            </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <h5>Your Password</h5>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
